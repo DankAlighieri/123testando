7600c00affa975d8838cda6a8d4b4d0e0735dcb0
marcusvrsousa <marcus.vrsousa@gmail.com>
2024-12-19 21:52:57 -0300

    feat: conversor de cm2 para m2 adicionado + estrutura de repeticao. removed: pasta output e arquivo .exe

--- conversor.c
+++ conversor.c
@@ -1,24 +1,47 @@
-// 7. Unidades de área (metro quadrado, centímetro quadrado)
-
 #include <stdio.h>
 #include <locale.h>
 
-float converter_cm2(float numero) {
-    float resultado = numero * 10000;
-    return resultado;
+double converter_para_cm2(double numero) {
+    return numero * 10000;
+}
+
+double converter_para_m2(double numero) {
+    return numero / 10000;
 }
 
 int main() {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
-    float valorMetroQuadrado, valorConvertido;
+    double valorInformado, valorConvertido;
+    int opcao = -1;
 
-    printf("Informe o valor em metros quadrados: ");
-    if (scanf("%f", &valorMetroQuadrado) != 1) {
-        printf("Entrada inválida. Por favor, insira um número.\n");
-        return 1;
-    }
+    do {
+        printf("Converter m2 em cm2 -- Digite 1 \n");
+        printf("Converter cm2 em m2 -- Digite 2 \n");
+        printf("Sair -- Digite 0 \n");
+        scanf("%d", &opcao);
 
-    valorConvertido = converter_cm2(valorMetroQuadrado);
-    printf("Valor em centímetros quadrados é: %.2f\n", valorConvertido);
+        if (opcao == 1) {
+            printf("Informe o valor em metros quadrados: ");
+            if (scanf("%lf", &valorInformado) != 1 || valorInformado <= 0) {
+                printf("Entrada invalida. Por favor, insira um numero maior que 0.\n");
+            } else {
+                valorConvertido = converter_para_cm2(valorInformado);
+                printf("Valor em centimetros quadrados: %f\n", valorConvertido);
+            }
+        } else if (opcao == 2) {
+            printf("Informe o valor em centimetros quadrados: ");
+            if (scanf("%lf", &valorInformado) != 1 || valorInformado <= 0) {
+                printf("Entrada invalida. Por favor, insira um numero maior que 0.\n");
+            } else {
+                valorConvertido = converter_para_m2(valorInformado);
+                printf("Valor em metros quadrados: %f\n", valorConvertido);
+            }
+        } else if (opcao == 0) {
+            printf("Saindo do programa.\n");
+        } else  {
+            printf("Opcao invalida. Por favor, selecione uma opcao valida.\n");
+        }
+
+    } while (opcao != 0);
+
     return 0;
 }
